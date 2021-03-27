bcadc07805cba4ab1ffcfef19a9c470a9c087863
idfoxdale <ishantkdas@hmail.com>
2021-03-27 13:03:35 +0530

    started function

--- Bitcoinmine.py
+++ Bitcoinmine.py
@@ -1,4 +1,13 @@
 # Coding for usual stuff Bitcoin mining, hmmm that's typical
 # how hashing looks like, bbay steps!!
 from hashlib import sha256
-print(sha256("ABC".encode("ascii")).hexdigest())
+# display sha256 for a string 
+# print(sha256("ABC".encode("ascii")).hexdigest())
+
+# deffing a function for
+def SHA256(text):
+    return sha256(text.encode("ascii")).hexdigest()
+
+if __name__ == '__main__':
+    transactions='''
+    
