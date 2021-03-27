287b971bc13df1045b56706df60dde577b341c48
idfoxdale <ishantkdas@hmail.com>
2021-03-27 12:55:00 +0530

    hex display added  Please enter the commit message for your changes. Lines starting

--- Bitcoinmine.py
+++ Bitcoinmine.py
@@ -1,4 +1,4 @@
 # Coding for usual stuff Bitcoin mining, hmmm that's typical
 # how hashing looks like, bbay steps!!
 from hashlib import sha256
-print(sha256("ABC".encode("ascii")))
+print(sha256("ABC".encode("ascii")).hexdigest())
